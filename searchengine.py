969070eb31372eb9eef2380978b922eccd598a92
fengbs <fengyuehan1989@163.com>
2016-04-29 21:31:15 +0800

    add query

--- searchengine.py
+++ searchengine.py
@@ -14,10 +14,32 @@ class crawler:
         self.con.commit()
     #获取条目id 如果条目不存在，加入数据库
     def getentryid(self,table,field,value,createnew = True):
-        return None
+        cur = self.con.execute("select rowid form %s where %s = '%s'"% (table ,field , value))
+        res = cur.fetchone()
+        if res == None:
+            cur = self.con.execute("insert into %s (%s) values ('%s')" % (table ,field ,value))
+        else:
+            return res[0]
+                
     #为每个网址建立索引
     def addtoindex(self,url,soup):
-        print('Indexing %s' % url)
+        if self.indexed(url):return
+        print('Indexing' + url)
+
+        #获取每个单词
+        text = self.gettextonly(soup)
+        words = self.separatewords(text)
+
+        #得到URL的ID
+        urlid = self.getentryid('urllist','url',url)
+
+        #将每个单词与该url关联
+        for i in range(len(words)):
+            word = words[i]
+            if word in ignorewords:continue
+            wordid = self.getentryid('wordlist','word',word)
+            self.con.execute("insert into wordlocation(urlid,wordid,location) values (%d,%d,%d)"% (urlid,wordid,i))
+            
     #在html中提取文字
     def gettextonly(self,soup):
         v=soup.string
@@ -36,6 +58,10 @@ class crawler:
         return [s.lower() for s in splitter.split(text) if s!='']
     #如果url已经建立索引，那么返回True
     def isindexed(self,url):
+        u = self.con.execute("select rowid form urllist where url = '%s'" %url).fetchone()
+        if u!=None:
+            v = self.con.execute('select * from wordlocation where urlid = %d' % u[0]).fetchone()
+            if v!=None:return True
         return False
     #添加一个关联两个网页的连接
     def addlinkref(self ,urlForm,urlTo,linkText):
@@ -62,6 +88,7 @@ class crawler:
                         if url[0:4] == 'http' and not self.isindexed(url):
                             newpages.add(url)
                         linkText = self.gettextonly(link)
+                        print('%s' % linkText)
                         self.addlinkref(page,url,linkText)
                 self.dbcommit()
             pages = newpages
@@ -80,6 +107,48 @@ class crawler:
         self.con.execute('create index urltoidx on link(toid)')
         self.con.execute('create index urlfromidx on link(fromid)')
         self.dbcommit()
+    def testforTextonly(self,soup):
+        str = self.gettextonly(soup)
+        print('%s' % str)
+class searcher:
+    def __init__(self,dbname):
+        self.con = sqlite.connect(dbname)
+    def __del__(self):
+        self.con.close()
+    def getmatchrows(self,q):
+        #构造查询的字符串
+        fieldlist = 'w0.urlid'
+        tablelist = ''
+        clauselist = ''
+        wordids = []
+
+        #根据空格拆分单词
+        words = q.split(' ')
+        tablenumber = 0
+
+        for word in words:
+            #获取单词id
+            wordid = self.con.execute(
+                "select rowid from wordlist where word = '%s'" % word).fetchone()
+            if wordrow != None:
+                wordid = wordrow[0]
+                wordids.append(wordid)
+                if tablenumber > 0:
+                    tablelist += ','
+                    clauselist +=' and '
+                    clauselist+='w%d.urlid = w%d.urlid and ' %  (tablenumber -1 ,tablenumber)
+                fieldlist += ',w%d.location' % tablenumber
+                tablelist += 'wordlocation w%d' % tablenumber
+                clauselist += 'w%d.wordid = %d' % (tablenumber,wordid)
+                tablenumber+=1
+            # 根据各个组分，建立查询
+            fullquery = 'select %s from %s where %s' % (fieldlist,tablelist,clauselist)
+            cur = self.con.execute(fullquery)
+            rows = [row for row in cur]
+
+            return rows , wordids
+        
+                    
         
     
     
